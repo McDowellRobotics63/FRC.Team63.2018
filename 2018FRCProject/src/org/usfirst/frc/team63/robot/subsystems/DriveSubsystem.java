1d9be9054ff07a8c808e6dc25f30b729a4beb39d
McDowellRobotics63 <bucholtz@mtsd.org>
2018-01-31 19:23:04 -0500

    DriveSubsystem Update
    Added import Robot.java line in AutoDriveFixedDistance and created partial talon initialization function in DriveSubsystem.

--- 2018FRCProject/src/org/usfirst/frc/team63/robot/subsystems/DriveSubsystem.java
+++ 2018FRCProject/src/org/usfirst/frc/team63/robot/subsystems/DriveSubsystem.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team63.robot.subsystems;
 
 import org.usfirst.frc.team63.robot.RobotMap;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -14,12 +16,12 @@ public class DriveSubsystem extends Subsystem {
 	 private WPI_TalonSRX LeftMaster = new WPI_TalonSRX(RobotMap.DRIVELEFTMASTER); 
 	 private WPI_TalonSRX RightMaster = new WPI_TalonSRX(RobotMap.DRIVERIGHTMASTER); 
 	 private WPI_TalonSRX LeftSlave = new WPI_TalonSRX(RobotMap.DRIVELEFTSLAVE); 
-	 private WPI_TalonSRX RightSlave = new WPI_TalonSRX(RobotMap.DRIVERIGHTSLAVE); 
+	 private WPI_TalonSRX RightSlave = new WPI_TalonSRX(RobotMap.DRIVERIGHTSLAVE);
 	 
-	
-	public void motorConfig() {
-		LeftMaster.configNominalOutputForward(0.0, RobotMap.kTimeoutMs);
-    	LeftMaster.configNominalOutputReverse(-0.0, RobotMap.kTimeoutMs);
+	 public DriveSubsystem() {
+		 TalonConfig();
+	 }
+
     	LeftMaster.configPeakOutputForward(+12.0, RobotMap.kTimeoutMs);    	    
     	LeftMaster.configPeakOutputReverse(-12.0, RobotMap.kTimeoutMs);  
     	
@@ -43,5 +45,16 @@ public class DriveSubsystem extends Subsystem {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    /*Called in constructor to ensure correct drive talon settings*/
+    private void TalonConfig() {
+    	//Makes talons force zero voltage across when zero output to resist motion
+    	LeftMaster.setNeutralMode(NeutralMode.Brake);
+    	LeftSlave.setNeutralMode(NeutralMode.Brake);
+    	RightMaster.setNeutralMode(NeutralMode.Brake);
+    	RightSlave.setNeutralMode(NeutralMode.Brake);
+    	
+    	LeftSlave.set(ControlMode.Follower, RobotMap.DRIVELEFTMASTER);
+    	RightSlave.set(ControlMode.Follower, RobotMap.DRIVERIGHTMASTER);
+    }
 }
-
