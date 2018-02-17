f0828e3802356cad2757b7eab0549bc31a2bc78d
JakeSmith222 <jakersmith222@gmail.com>
2018-02-17 11:33:51 -0500

    Do the elevator limits in native units

--- 2018FRCProject/src/org/usfirst/frc/team63/robot/RobotMap.java
+++ 2018FRCProject/src/org/usfirst/frc/team63/robot/RobotMap.java
@@ -52,6 +52,7 @@ public class RobotMap {
 	//Lift constants
 	public static final double LIFT_INCHES_PER_UNIT = 0.000033;
 	public static final double MAX_LIFT_DISPLACEMENT_INCHES = 70;
+	public static final int MAX_LIFT_DISPLACEMENT_UNITS = (int)(MAX_LIFT_DISPLACEMENT_INCHES * LIFT_INCHES_PER_UNIT);
 	public static final double BOX_HEIGHT_INCHES = 11;
 	
 	//Controller Map
--- 2018FRCProject/src/org/usfirst/frc/team63/robot/subsystems/LiftSubsystem.java
+++ 2018FRCProject/src/org/usfirst/frc/team63/robot/subsystems/LiftSubsystem.java
@@ -96,7 +96,11 @@ public class LiftSubsystem extends Subsystem {
     		appliedFeedFoward = Direction.UP;
     	}
     		
-    	liftMotor.set(ControlMode.MotionMagic, Math.max(0, Math.min(setpoint_units, RobotMap.MAX_LIFT_DISPLACEMENT_INCHES)));;
+    	setpoint_units = Math.max(0, Math.min(setpoint_units, RobotMap.MAX_LIFT_DISPLACEMENT_UNITS));
+    	
+    	SmartDashboard.putNumber("setpoint_units", setpoint_units);
+    	
+    	liftMotor.set(ControlMode.MotionMagic, setpoint_units);
     }
     
     public boolean isMotionMagicNearTarget() {
