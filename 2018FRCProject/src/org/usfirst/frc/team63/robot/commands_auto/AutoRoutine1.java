2d3ada5778eee92b0c868f841a1da6377e854a68
dan & jake <dansong0729@gmail.com>
2018-03-01 14:03:56 -0500

    Debug mode for AutoRoutine1
    put auto into pseudo debug mode

--- 2018FRCProject/src/org/usfirst/frc/team63/robot/commands_auto/AutoRoutine1.java
+++ 2018FRCProject/src/org/usfirst/frc/team63/robot/commands_auto/AutoRoutine1.java
@@ -18,16 +18,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoRoutine1 extends CommandGroup {
     //Das Auto, all inches
 	public static final double SWITCH_HEIGHT = 15;
-    public static final double DIST_SCALE = 300; //Distance from the starting point to the scale
+    public static final double DIST_SCALE0 = 205;
+    public static final double DIST_SCALE1 = 36;
     public static final double DIST_SWITCH = 140; //Distance from the starting point to the switch
-    public static final double DIST_LINE = 0; //line to cross in auto for points
+    public static final double DIST_WE_REKT_BOIS = 90; //line to cross in auto for points
     public static final double DIST_UNTIL_TURN = 70; //how far to go before turning when in middle
 	public static final double DIST_HALF_SWITCH_WIDTH = 60; //horizontal distance for robot in middle to go
 	public static final double DIST_TWOCUBE0 = 48; //distance to come back for switch before turning
 	public static final double DIST_TWOCUBE1 = 90.50966799187808; //slanted bit
 	public static final double DIST_TWOCUBE2 = 32; //drive forward for cube
 	
-	private char botPos; //l, m, or r
+	private char botPos = 'z'; //l, m, or r
 	
     public AutoRoutine1(String fieldSetup, int switches) {
     	System.out.println("Switches: " + switches);
@@ -51,32 +52,32 @@ public class AutoRoutine1 extends CommandGroup {
     		} else {
     			addSequential(new AutoRotate(-90));
     		}
-    		addSequential(new AutoDriveFixedDistance(DIST_SWITCH-DIST_UNTIL_TURN));
+    		addSequential(new AutoDriveFixedDistance(DIST_SWITCH-DIST_UNTIL_TURN-20));
     		addSequential(new BoxShootForTime());
     	} else if(fieldSetup.charAt(1) == botPos) { //scale on our bot's side
     		//go for scale
     		System.out.println("Goooooooooooo");
-    		addParallel(new MoveLiftMaxHeight());
-    		addSequential(new AutoDriveFixedDistance(DIST_SCALE));
+    		addSequential(new AutoDriveFixedDistance(DIST_SCALE0));
     		System.out.println("Already Went");
-    		turnCalc(90);
-        	addSequential(new AutoDriveFixedDistance(24));
+    		turnCalc(45);
+    		addParallel(new AutoDriveFixedDistance(DIST_SCALE1));
+        	addSequential(new MoveLiftMaxHeight());
     		addSequential(new BoxShootForTime());
-    		addSequential(new AutoDriveFixedDistance(-24));
-    		if (fieldSetup.charAt(0) == botPos) { //switch is also on our bot's side
-    			//continue for two cube auto
-        		turnCalc(90);
-        		addSequential(new AutoDriveFixedDistance(DIST_TWOCUBE0));
-        		turnCalc(-45);
-            	addParallel(new MoveLiftMinHeight());
-        		addSequential(new AutoDriveFixedDistance(DIST_TWOCUBE1));
-        		turnCalc(45);
-        		addSequential(new AutoDriveFixedDistance(DIST_TWOCUBE2));
-        		addSequential(new BoxObtainFoSho());
-        		addParallel(new AutoSetLiftPosition(SWITCH_HEIGHT));
-        		addSequential(new AutoDriveFixedDistance(26));
-        		addSequential(new BoxShootForTime());
-    		}
+    		addSequential(new AutoDriveFixedDistance(-DIST_SCALE1));
+//    		if (fieldSetup.charAt(0) == botPos) { //switch is also on our bot's side
+//    			//continue for two cube auto
+//        		turnCalc(90);
+//        		addSequential(new AutoDriveFixedDistance(DIST_TWOCUBE0));
+//        		turnCalc(-45);
+//            	addParallel(new MoveLiftMinHeight());
+//        		addSequential(new AutoDriveFixedDistance(DIST_TWOCUBE1));
+//        		turnCalc(45);
+//        		addSequential(new AutoDriveFixedDistance(DIST_TWOCUBE2));
+//        		addSequential(new BoxObtainFoSho());
+//        		addParallel(new AutoSetLiftPosition(SWITCH_HEIGHT));
+//        		addSequential(new AutoDriveFixedDistance(26));
+//        		addSequential(new BoxShootForTime());
+//    		}
     	}
     	else if (fieldSetup.charAt(0) == botPos) { //only switch in on same side
     		//just switch
@@ -89,7 +90,7 @@ public class AutoRoutine1 extends CommandGroup {
     	}
     	else { //we rekt bois
     		//just drive forward
-    		addSequential(new AutoDriveFixedDistance(DIST_LINE));
+    		addSequential(new AutoDriveFixedDistance(DIST_WE_REKT_BOIS));
     	}
     	addSequential(new MoveLiftMinHeight());
     }
