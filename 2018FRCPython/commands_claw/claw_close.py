08e9532933a12a3da92b30ed99c73afaf9ab7861
joewak <joe.wakeman@gmail.com>
2018-12-15 11:31:12 -0500

    Add remaining claw commands

---
+++ 2018FRCPython/commands_claw/claw_close.py
@@ -0,0 +1,17 @@
+from wpilib.command import Command
+
+import robotmap
+
+class ClawClose(Command):
+    def __init__(self, robot):
+        super().__init__()
+        self.robot = robot
+        self.requires(robot.claw)
+
+    # Called repeatedly when this Command is scheduled to run
+    def execute(self):
+        self.robot.claw.close()
+
+    # Make this return true when this Command no longer needs to run execute()
+    def isFinished(self):
+        return True
